7f19737ac300d2c4cfa9a6bcbce228a4040f4989
Minota M Dache <minotadache@gmail.com>
2025-04-15 15:16:58 +0200

    urdf commit

---
+++ amr_swarming_description/launch/display.launch.py
@@ -0,0 +1,39 @@
+
+from launch import LaunchDescription
+from launch_ros.parameter_descriptions import ParameterValue
+from launch_ros.actions import Node 
+import os
+from launch.substitutions import Command 
+from ament_index_python.packages import get_package_share_path
+# we first define the launch description
+
+def generate_launch_description():
+    
+    urdf_file = os.path.join(get_package_share_path('amr_swarming_description'), 
+                             'urdf', 'mrobot.urdf')
+    robot_description = ParameterValue(Command(['xacro ', urdf_file]), value_type=str)
+
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        parameters=[{'robot_description': robot_description}]
+
+    )
+
+    joint_state_publisher_node = Node(
+        package="joint_state_publisher_gui",
+        executable="joint_state_publisher_gui"
+    )
+
+    rviz2_node = Node(
+        package="rviz2",
+        executable="rviz2"
+
+    )   
+
+    return LaunchDescription([
+        robot_state_publisher_node,
+        joint_state_publisher_node,
+        rviz2_node
+
+    ])
